98d8e3c0734047535fead26ecc39d7ba5234b218
Robbie Edwards <rob.a.edwards@gmail.com>
2022-12-24 23:56:10 -0500

    release 1.0

--- mayabox.ino
+++ mayabox.ino
@@ -2,11 +2,12 @@
 #include "MPU9250.h"
 #include "Adafruit_VL6180X.h"
 #include <Wire.h>
-//#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h>
+#include <PWMServo.h>
 
 
 #define AHRS false         // Set to false for basic data read
-#define SerialDebug true  // Set to true to get Serial output for debugging
+#define SerialDebug false  // Set to true to get Serial output for debugging
 
 #define LED_NEO_PIN 1
 #define NEO_NUMPIXELS 48
@@ -15,14 +16,41 @@
 #define MPU9250_ADDRESS MPU9250_ADDRESS_AD0
 
 
-
 MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
 Adafruit_VL6180X vl = Adafruit_VL6180X();
-//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_NUMPIXELS, LED_NEO_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_NUMPIXELS, LED_NEO_PIN, NEO_GRB + NEO_KHZ800);
 
 
-//horizontal LEDS 15
-//vertical LEDS 9
+float LEDarr[48][2] = {{-.875,1.0},{-.75,1.0},{-.625,1.0},{-.5,1.0},{-.375,1.0},{-.25,1.0},{-.125,1.0},{0,1.0},{.125,1.0},{.25,1.0},{.375,1.0},{.5,1.0},{.625,1.0},{.75,1.0},{.875,1.0},
+                      {1.0,0.8},{1.0,0.6},{1.0,0.4},{1.0,0.2},{1.0,0},{1.0,-0.2},{1.0,-0.4},{1.0,-0.6},{1.0,-0.8},
+                      {.875,-1.0},{.75,-1.0},{.625,-1.0},{.5,-1.0},{.375,-1.0},{.25,-1.0},{.125,-1.0},{0,-1.0},{-.125,-1.0},{-.25,-1.0},{-.375,-1.0},{-.5,-1.0},{-.625,-1.0},{-.75,-1.0},{-.875,-1.0},
+                      {-1.0,-0.8},{-1.0,-0.6},{-1.0,-0.4},{-1.0,-0.2},{-1.0,0},{-1.0,0.2},{-1.0,0.4},{-1.0,0.6},{-1.0,0.8}};
+
+
+const int estopbuttonPin=2;
+const int ksw1Pin = 3;
+const int ksw2Pin = 4;
+const int button1Pin = 9;
+const int button1LedPin = 8;
+const int button2Pin = 11;
+const int button2LedPin = 10;
+const int button3Pin = 13;
+const int button3LedPin = 12;
+const int button4Pin = 14;
+const int button4LedPin = 15;
+const int button5Pin = 16;
+const int button5LedPin = 17;
+const int led1Pin =5;
+const int led2Pin =6;
+const int led3Pin =7;
+const int redbuttonPin= 20;
+const int bigbuttonPin = 0;
+PWMServo myservo; 
+const int servoPin = 23;
+const int motorinA = 22;
+const int motorinB = 21;
+
+int lastrange = 186;
 
 
 void setup() {
@@ -56,7 +84,7 @@ void setup() {
     Serial.print(myIMU.selfTest[5],1); Serial.println("% of factory value");
 
     // Calibrate gyro and accelerometers, load biases in bias registers
-    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
+    //myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
 
     myIMU.initMPU9250();
     // Initialize device for active mode read of acclerometer, gyroscope, and
@@ -103,20 +131,19 @@ void setup() {
 
     // The next call delays for 4 seconds, and then records about 15 seconds of
     // data to calculate bias and scale.
-    //    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
-    Serial.println("AK8963 mag biases (mG)");
-    Serial.println(myIMU.magBias[0]);
-    Serial.println(myIMU.magBias[1]);
-    Serial.println(myIMU.magBias[2]);
-
-    Serial.println("AK8963 mag scale (mG)");
-    Serial.println(myIMU.magScale[0]);
-    Serial.println(myIMU.magScale[1]);
-    Serial.println(myIMU.magScale[2]);
-    //    delay(2000); // Add delay to see results before serial spew of data
-
     if(SerialDebug)
     {
+    //    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
+      Serial.println("AK8963 mag biases (mG)");
+      Serial.println(myIMU.magBias[0]);
+      Serial.println(myIMU.magBias[1]);
+      Serial.println(myIMU.magBias[2]);
+
+      Serial.println("AK8963 mag scale (mG)");
+      Serial.println(myIMU.magScale[0]);
+      Serial.println(myIMU.magScale[1]);
+      Serial.println(myIMU.magScale[2]);
+    //    delay(2000); // Add delay to see results before serial spew of data
       Serial.println("Magnetometer:");
       Serial.print("X-Axis sensitivity adjustment value ");
       Serial.println(myIMU.factoryMagCalibration[0], 2);
@@ -126,21 +153,128 @@ void setup() {
       Serial.println(myIMU.factoryMagCalibration[2], 2);
     }
   }
-    while (!Serial) {
+  /*
+  while (!Serial) 
+  {
     delay(1);
   }
+  */
   //VL6180X RANGE SENSOR
   Serial.println("Adafruit VL6180x test!");
-  if (! vl.begin()) {
+  if (! vl.begin()) 
+  {
     Serial.println("Failed to find sensor");
     while (1);
   }
   Serial.println("Sensor found!");
   //NEO PIXEL INIT
-  //pixels.begin(); 
+  pixels.begin(); 
+
+//BUTTONS
+  pinMode(estopbuttonPin,INPUT);
+  pinMode(ksw1Pin,INPUT);
+  pinMode(ksw2Pin,INPUT);
+  pinMode(button1Pin,INPUT);
+  pinMode(button1LedPin,OUTPUT);
+  pinMode(button2Pin,INPUT);
+  pinMode(button2LedPin,OUTPUT);
+  pinMode(button3Pin,INPUT);
+  pinMode(button3LedPin,OUTPUT);
+  pinMode(button4Pin,INPUT);
+  pinMode(button4LedPin,OUTPUT);
+  pinMode(button5Pin,INPUT);
+  pinMode(button5LedPin,OUTPUT);
+  pinMode(led1Pin,OUTPUT);
+  pinMode(led2Pin,OUTPUT);
+  pinMode(led3Pin,OUTPUT);
+  pinMode(redbuttonPin,INPUT);
+  pinMode(bigbuttonPin,0);
+  pinMode(motorinA,OUTPUT);
+  pinMode(motorinB,OUTPUT);
+  myservo.attach(servoPin);
+
+
 }
 
 void loop() {
+
+  // READ BUTTONS
+  int ES = digitalRead(estopbuttonPin);
+  int ksw1 = digitalRead(ksw1Pin);
+  int ksw2 = digitalRead(ksw2Pin);
+  int but1 = digitalRead(button1Pin);
+  int but2 = digitalRead(button2Pin);
+  int but3 = digitalRead(button3Pin);
+  int but4 = digitalRead(button4Pin);
+  int but5 = digitalRead(button5Pin);
+  int redbut = digitalRead(redbuttonPin);
+  int bigbut = digitalRead(bigbuttonPin);
+
+  //KSW
+  if (ksw1 == false && ksw2 == false){
+    digitalWrite(led1Pin, HIGH);
+    digitalWrite(led2Pin, LOW);
+    digitalWrite(led3Pin, LOW);
+  }
+  else if  (ksw1 == true && ksw2 == false){
+    digitalWrite(led1Pin, LOW);
+    digitalWrite(led2Pin, HIGH);
+    digitalWrite(led3Pin, LOW);
+  }
+  else if  (ksw2 == true && ksw1 == true){
+    digitalWrite(led1Pin, LOW);
+    digitalWrite(led2Pin, LOW);
+    digitalWrite(led3Pin, HIGH);
+  }
+  // SILVER Push buttons and LEDS
+  if (but1 == HIGH){
+    digitalWrite(button1LedPin, HIGH);
+  }
+  else{
+    digitalWrite(button1LedPin, LOW);
+  }
+  if (but2 == HIGH){
+    digitalWrite(button2LedPin, HIGH);
+  }
+  else{
+    digitalWrite(button2LedPin, LOW);
+  }
+  if (but3 == HIGH){
+    digitalWrite(button3LedPin, HIGH);
+  }
+  else{
+    digitalWrite(button3LedPin, LOW);
+  }
+  if (but4 == HIGH){
+    digitalWrite(button4LedPin, HIGH);
+  }
+  else{
+    digitalWrite(button4LedPin, LOW);
+  }
+  if (but5 == HIGH){
+    digitalWrite(button5LedPin, HIGH);
+  }
+  else{
+    digitalWrite(button5LedPin, LOW);
+  }
+  // 
+  if (bigbut == HIGH && redbut == HIGH && ES == true)
+  {
+    digitalWrite(motorinA,LOW);
+    digitalWrite(motorinB,HIGH);
+  }
+  else  if (bigbut == HIGH && redbut == LOW && ES == true)
+  {
+    digitalWrite(motorinA,HIGH);
+    digitalWrite(motorinB,LOW);
+  }
+  else  if (bigbut == LOW)
+  {
+    digitalWrite(motorinA,LOW);
+    digitalWrite(motorinB,LOW);
+  }
+  //Serial.print("redBut ");Serial.println(redbut);
+
   // put your main code here, to run repeatedly:
   if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
   {
@@ -191,18 +325,47 @@ void loop() {
 
   //VL6180X Range Sensor
   float lux = vl.readLux(VL6180X_ALS_GAIN_5);
-  uint8_t range = vl.readRange();
-  uint8_t status = vl.readRangeStatus();
-
+  //uint8_t range = vl.readRange();
+  //uint8_t status = vl.readRangeStatus();
+  int range = vl.readRange();
+  int status = vl.readRangeStatus();
   //NEOPIXELS
-  /*
-  for(int i=0;i<NEO_NUMPIXELS;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(100,0,150)); // Moderately bright green color.
-    //delay(delayval); // Delay for a period of time (in milliseconds).
+
+  //myIMU.ax Gs, measured from -1 to 1
+  //myIMU.ay Gs, measured from -1 to 1
+  //LEDarr
+  //Serial.println(status);
+  if (ES==true)
+  {
+    int red = 0;
+    if (status > 0){
+      red = 8;
+    }
+    else {
+      red = (185-range)/2;
+    }
+    
+    for(int i=0;i<NEO_NUMPIXELS;i++){
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      float r = 1/sqrt(pow(LEDarr[i][0]-(-myIMU.ay),2)+pow(LEDarr[i][1]-(-myIMU.ax),2));
+      pixels.setPixelColor(i, pixels.Color(red,0,int(max(r-0.5,0)*30.0)));
+    }
+
+    if ((abs(range - lastrange) > 5) && (but1 == HIGH))
+    {
+      myservo.write(max(10,min(range,175)));
+    }
+    lastrange=range;
+  }
+  else
+  {
+    for(int i=0;i<NEO_NUMPIXELS;i++){
+      pixels.setPixelColor(i, pixels.Color(50,0,0));
+    }
   }
   pixels.show(); // This sends the updated pixel color to the hardware.
-  */
+
+
   if (!AHRS)
   {
     myIMU.delt_t = millis() - myIMU.count;
